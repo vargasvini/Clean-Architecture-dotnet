f540df8d590967213e439e1769a6bc09c276edad
Vinícius Vargas Ribeiro <viniciusvargas@betalearning.com.br>
2021-07-28 11:37:59 -0300

    Criação das interfaces do repositório na camada de domínio

---
+++ Core/Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Core.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int? id);
+        Task<Category> InsertAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> DeleteAsync(Category category);
+    }
+}
---
+++ Core/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Core.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> InsertAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> DeleteAsync(Product product);
+    }
+}
