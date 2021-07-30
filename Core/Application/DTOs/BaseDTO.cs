42c8a59ad6d34b3b5ff8575f65d2888ca5c3770c
Vinícius Vargas Ribeiro <viniciusvargas@betalearning.com.br>
2021-07-30 19:54:08 -0300

    Criação de uma base dto com datas e responsáveis por alterações

---
+++ Core/Application/DTOs/BaseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CleanArchitecture.Core.Application.DTOs
+{
+    public class BaseDTO
+    {
+        public BaseDTO()
+        {
+            CreatedAt = DateTime.Now;
+            ModifiedAt = DateTime.Now;
+            CreatedBy = "Vini";
+            ModifiedBy = "Vini";
+        }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime ModifiedAt { get; protected set; }
+        public string CreatedBy { get; protected set; }
+        public string ModifiedBy { get; protected set; }
+    }
+}
--- Core/Application/DTOs/CategoryDTO.cs
+++ Core/Application/DTOs/CategoryDTO.cs
@@ -3,15 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CleanArchitecture.Core.Application.DTOs
 {
-    public class CategoryDTO
+    public class CategoryDTO : BaseDTO
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "Name is required")]
         [MinLength(3)]
         [MaxLength(100)]
         public string Name { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public string CreatedBy { get; set; }
-        public string ModifiedBy { get; set; }
     }
 }
