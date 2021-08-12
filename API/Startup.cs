4a5e03191f98ca84c6b28305a680eb18ae7789a9
vargasvini <vargasrvinicius@gmail.com>
2021-08-12 10:05:51 -0300

    Adicionando docker + kubernetes

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,9 +41,10 @@ namespace CleanArchitecture.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchitecture.API v1"));
+                
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchitecture.API v1"));
 
             app.UseHttpsRedirection();
 
